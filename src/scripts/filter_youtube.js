4702282db4582f1e61416a07e567d43e203d25a0
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-02-09 17:26:57 +0530

    fix: query page if only on tab

--- src/scripts/filter_youtube.js
+++ src/scripts/filter_youtube.js
@@ -5,13 +5,13 @@
 (function() {
     const youtubeUrl = 'https://www.youtube.com';
 
-    // get search results
-    const list = document.querySelectorAll('.rc .r');
-
     // Test at: https://regex101.com/r/dxeCFd/1
     if (/\btbm=vid\b/i.test(window.location.search)) {
+        // get search results
+        const list = document.querySelectorAll('.rc .r');
+
         if (list.length > 0) {
-            console.log('just-YouTube: Showing YouTube results only');
+            console.log('just-YouTube: Showing YouTube results only'); // eslint-disable-line no-console
 
             list.forEach(entry => {
                 if (entry.firstChild.origin !== youtubeUrl) {
