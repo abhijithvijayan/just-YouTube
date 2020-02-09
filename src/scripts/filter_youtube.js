5afbe61b9c0ac17a1f01b0d64ee46024a945f68a
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-02-09 13:47:58 +0530

    chore: show a status text

--- src/scripts/filter_youtube.js
+++ src/scripts/filter_youtube.js
@@ -11,6 +11,8 @@
     // Test at: https://regex101.com/r/dxeCFd/1
     if (/\btbm=vid\b/i.test(window.location.search)) {
         if (list.length > 0) {
+            console.log('just-YouTube: Showing YouTube results only');
+
             list.forEach(entry => {
                 if (entry.firstChild.origin !== youtubeUrl) {
                     entry.parentNode.style.display = 'none';
